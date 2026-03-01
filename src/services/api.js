dfa2f57e9eec84e0e30bfe0ad2338d8f167eaf80
aasmitha01 <tommandruaasmitha@gmail.com>
2026-03-01 21:28:36 +0530

    SpendSense Frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `https://personal-finance-dashboard-backend-34av.onrender.com/`,
+  baseURL: "https://personal-finance-dashboard-backend-34av.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
